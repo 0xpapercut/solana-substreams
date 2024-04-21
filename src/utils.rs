0f4d4ce0a1d9d24e566b8404f0161da541e403ee
0xpapercut <0xpapercut@gmail.com>
2024-04-21 17:10:34 +0200

    Add utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+use substreams_solana::pb::sf::solana::r#type::v1::{CompiledInstruction, ConfirmedTransaction, InnerInstruction, Transaction, TransactionStatusMeta};
+
+pub struct TokenAccount {
+    pub owner: String,
+    pub mint: String,
+}
+
+pub fn get_token_accounts(transaction: &ConfirmedTransaction) -> HashMap<String, TokenAccount> {
+    let meta = transaction.meta.as_ref().unwrap();
+    let accounts = transaction.resolved_accounts_as_strings();
+
+    let mut map: HashMap<String, TokenAccount> = HashMap::new();
+    for token_balance in &meta.pre_token_balances {
+        let address = &accounts[token_balance.account_index as usize];
+        map.insert(address.clone(), TokenAccount {
+            mint: token_balance.mint.clone(),
+            owner: token_balance.owner.clone(),
+        });
+    }
+    map
+}
