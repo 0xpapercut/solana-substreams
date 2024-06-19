9cce1ac166038eae99276fcc383fd4653618681f
0xpapercut <0xpapercut@gmail.com>
2024-06-20 00:14:45 +0200

    Update token.sh

--- token.sh
+++ token.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -z "$STREAMINGFAST_KEY" ]; then
+    echo "Error: STREAMINGFAST_KEY is not set."
+    exit 1
+fi
+
 export SUBSTREAMS_API_TOKEN=$(curl https://auth.streamingfast.io/v1/auth/issue -s --data-binary '{"api_key":"'$STREAMINGFAST_KEY'"}' | jq -r .token)
 echo "Token set on in SUBSTREAMS_API_TOKEN"
