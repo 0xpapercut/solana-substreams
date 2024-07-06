c7b7e8aceacc316035ac5be5367fce9f7374e936
aaran <nabeelnaveed98@icloud.com>
2024-07-06 15:39:17 +0500

    fix imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,25 @@
 use substreams::errors::Error;
-use substreams_solana::pb::sf::solana::r#type::v1::ConfirmedTransaction;
 use substreams_solana::pb::sf::solana::r#type::v1::Block;
-use structured_instructions::{get_structured_instructions, StructuredInstruction, StructuredInstructions};
+use substreams_solana::pb::sf::solana::r#type::v1::ConfirmedTransaction;
+use substreams_solana_structured_instructions::{
+    get_structured_instructions, StructuredInstruction, StructuredInstructions,
+};
 
-use substreams_solana_raydium_amm as raydium_amm;
 use raydium_amm::instruction::AmmInstruction;
 use raydium_amm::RAYDIUM_LIQUIDITY_POOL;
+use substreams_solana_raydium_amm as raydium_amm;
 
-use spl_token_substream;
 use bs58;
+use spl_token_substream;
 mod pb;
 
-use substreams_solana_utils::{
-    TransactionContext,
-    ConfirmedTransactionExt,
-};
+use substreams_solana_utils::{ConfirmedTransactionExt, TransactionContext};
 
 #[substreams::handlers::map]
 pub fn raydium_events(block: Block) -> Result<pb::raydium::BlockRaydiumEvents, Error> {
     let slot = block.slot;
     let transactions = parse_block(block);
-    Ok(pb::raydium::BlockRaydiumEvents {
-        transactions,
-        slot,
-    })
+    Ok(pb::raydium::BlockRaydiumEvents { transactions, slot })
 }
 
 pub fn parse_block(block: Block) -> Vec<pb::raydium::TransactionRaydiumEvents> {
@@ -50,13 +46,19 @@ fn parse_transaction(transaction: &ConfirmedTransaction) -> Vec<pb::raydium::Ray
     }
 
     for instruction in instructions.flattened() {
-        if bs58::encode(context.get_account_from_index(instruction.program_id_index as usize)).into_string() != RAYDIUM_LIQUIDITY_POOL {
+        if bs58::encode(context.get_account_from_index(instruction.program_id_index as usize))
+            .into_string()
+            != RAYDIUM_LIQUIDITY_POOL
+        {
             continue;
         }
         match parse_instruction(&instruction, &context) {
             Ok(Some(event)) => events.push(event),
             Ok(None) => (),
-            Err(error) => substreams::log::println(format!("Failed to process instruction of transaction {}: {}", &context.signature, error))
+            Err(error) => substreams::log::println(format!(
+                "Failed to process instruction of transaction {}: {}",
+                &context.signature, error
+            )),
         }
     }
     events
@@ -64,23 +66,25 @@ fn parse_transaction(transaction: &ConfirmedTransaction) -> Vec<pb::raydium::Ray
 
 pub fn parse_instruction(
     instruction: &StructuredInstruction,
-    context: &TransactionContext
+    context: &TransactionContext,
 ) -> Result<Option<pb::raydium::RaydiumEvent>, String> {
-    if bs58::encode(context.get_account_from_index(instruction.program_id_index as usize)).into_string() != RAYDIUM_LIQUIDITY_POOL {
+    if bs58::encode(context.get_account_from_index(instruction.program_id_index as usize))
+        .into_string()
+        != RAYDIUM_LIQUIDITY_POOL
+    {
         return Err("Not a Raydium instruction.".to_string());
     }
     let unpacked = AmmInstruction::unpack(&instruction.data)?;
     match unpacked {
-        AmmInstruction::SwapBaseIn(_) |
-        AmmInstruction::SwapBaseOut(_) => {
+        AmmInstruction::SwapBaseIn(_) | AmmInstruction::SwapBaseOut(_) => {
             parse_swap_instruction(instruction, context).map(Some)
-        },
+        }
         AmmInstruction::Initialize2(initialize) => {
             parse_initialize_instruction(instruction, context, initialize.nonce).map(Some)
-        },
+        }
         AmmInstruction::Deposit(_deposit) => {
             parse_deposit_instruction(instruction, context).map(Some)
-        },
+        }
         AmmInstruction::Withdraw(_withdraw) => {
             parse_withdraw_instruction(instruction, context).map(Some)
         }
@@ -92,12 +96,22 @@ fn parse_swap_instruction(
     instruction: &StructuredInstruction,
     context: &TransactionContext,
 ) -> Result<pb::raydium::RaydiumEvent, String> {
-    let amm = bs58::encode(context.get_account_from_index(instruction.accounts[1] as usize)).into_string();
-    let user = bs58::encode(context.get_account_from_index(*instruction.accounts.last().unwrap() as usize)).into_string();
+    let amm = bs58::encode(context.get_account_from_index(instruction.accounts[1] as usize))
+        .into_string();
+    let user = bs58::encode(
+        context.get_account_from_index(*instruction.accounts.last().unwrap() as usize),
+    )
+    .into_string();
 
     let instructions_len = instruction.inner_instructions.len();
-    let transfer_in = parse_token_transfer_instruction(&instruction.inner_instructions[instructions_len - 2], context)?;
-    let transfer_out = parse_token_transfer_instruction(&instruction.inner_instructions[instructions_len - 1], context)?;
+    let transfer_in = parse_token_transfer_instruction(
+        &instruction.inner_instructions[instructions_len - 2],
+        context,
+    )?;
+    let transfer_out = parse_token_transfer_instruction(
+        &instruction.inner_instructions[instructions_len - 1],
+        context,
+    )?;
 
     let data = pb::raydium::SwapData {
         amount_in: transfer_in.amount,
@@ -117,13 +131,24 @@ fn parse_initialize_instruction(
     context: &TransactionContext,
     nonce: u8,
 ) -> Result<pb::raydium::RaydiumEvent, String> {
-    let amm = bs58::encode(context.get_account_from_index(instruction.accounts[4] as usize)).into_string();
-    let user = bs58::encode(context.get_account_from_index(instruction.accounts[17] as usize)).into_string();
+    let amm = bs58::encode(context.get_account_from_index(instruction.accounts[4] as usize))
+        .into_string();
+    let user = bs58::encode(context.get_account_from_index(instruction.accounts[17] as usize))
+        .into_string();
 
     let instructions_len = instruction.inner_instructions.len();
-    let coin_transfer = parse_token_transfer_instruction(&instruction.inner_instructions[instructions_len - 3], context)?;
-    let pc_transfer = parse_token_transfer_instruction(&instruction.inner_instructions[instructions_len - 2], context)?;
-    let lp_mint_to = parse_token_mint_to_instruction(&instruction.inner_instructions[instructions_len - 1], context)?;
+    let coin_transfer = parse_token_transfer_instruction(
+        &instruction.inner_instructions[instructions_len - 3],
+        context,
+    )?;
+    let pc_transfer = parse_token_transfer_instruction(
+        &instruction.inner_instructions[instructions_len - 2],
+        context,
+    )?;
+    let lp_mint_to = parse_token_mint_to_instruction(
+        &instruction.inner_instructions[instructions_len - 1],
+        context,
+    )?;
 
     let data = pb::raydium::InitializeData {
         pc_init_amount: pc_transfer.amount,
@@ -143,15 +168,26 @@ fn parse_initialize_instruction(
 
 fn parse_deposit_instruction(
     instruction: &StructuredInstruction,
-    context: &TransactionContext
+    context: &TransactionContext,
 ) -> Result<pb::raydium::RaydiumEvent, String> {
-    let amm = bs58::encode(context.get_account_from_index(instruction.accounts[1] as usize)).into_string();
-    let user = bs58::encode(context.get_account_from_index(instruction.accounts[12] as usize)).into_string();
+    let amm = bs58::encode(context.get_account_from_index(instruction.accounts[1] as usize))
+        .into_string();
+    let user = bs58::encode(context.get_account_from_index(instruction.accounts[12] as usize))
+        .into_string();
 
     let instructions_len = instruction.inner_instructions.len();
-    let coin_transfer = parse_token_transfer_instruction(&instruction.inner_instructions[instructions_len - 3], context)?;
-    let pc_transfer = parse_token_transfer_instruction(&instruction.inner_instructions[instructions_len - 2], context)?;
-    let lp_mint_to = parse_token_mint_to_instruction(&instruction.inner_instructions[instructions_len - 1], context)?;
+    let coin_transfer = parse_token_transfer_instruction(
+        &instruction.inner_instructions[instructions_len - 3],
+        context,
+    )?;
+    let pc_transfer = parse_token_transfer_instruction(
+        &instruction.inner_instructions[instructions_len - 2],
+        context,
+    )?;
+    let lp_mint_to = parse_token_mint_to_instruction(
+        &instruction.inner_instructions[instructions_len - 1],
+        context,
+    )?;
 
     let data = pb::raydium::DepositData {
         pc_amount: pc_transfer.amount,
@@ -169,13 +205,24 @@ fn parse_withdraw_instruction(
     instruction: &StructuredInstruction,
     context: &TransactionContext,
 ) -> Result<pb::raydium::RaydiumEvent, String> {
-    let amm = bs58::encode(context.get_account_from_index(instruction.accounts[1] as usize)).into_string();
-    let user = bs58::encode(context.get_account_from_index(instruction.accounts[16] as usize)).into_string();
+    let amm = bs58::encode(context.get_account_from_index(instruction.accounts[1] as usize))
+        .into_string();
+    let user = bs58::encode(context.get_account_from_index(instruction.accounts[16] as usize))
+        .into_string();
 
     let instructions_len = instruction.inner_instructions.len();
-    let coin_transfer = parse_token_transfer_instruction(&instruction.inner_instructions[instructions_len - 3], context)?;
-    let pc_transfer = parse_token_transfer_instruction(&instruction.inner_instructions[instructions_len - 2], context)?;
-    let lp_mint_to = parse_token_burn_instruction(&instruction.inner_instructions[instructions_len - 1], context)?;
+    let coin_transfer = parse_token_transfer_instruction(
+        &instruction.inner_instructions[instructions_len - 3],
+        context,
+    )?;
+    let pc_transfer = parse_token_transfer_instruction(
+        &instruction.inner_instructions[instructions_len - 2],
+        context,
+    )?;
+    let lp_mint_to = parse_token_burn_instruction(
+        &instruction.inner_instructions[instructions_len - 1],
+        context,
+    )?;
 
     let data = pb::raydium::WithdrawData {
         pc_amount: pc_transfer.amount,
@@ -195,10 +242,12 @@ fn parse_token_transfer_instruction(
 ) -> Result<spl_token_substream::pb::spl_token::TransferEvent, String> {
     match spl_token_substream::parse_instruction(instruction, context) {
         Ok(Some(event)) => match event {
-            spl_token_substream::pb::spl_token::spl_token_event::Event::Transfer(transfer) => Ok(transfer),
-            _ => Err("Not an SPL Token transfer event.".to_string())
+            spl_token_substream::pb::spl_token::spl_token_event::Event::Transfer(transfer) => {
+                Ok(transfer)
+            }
+            _ => Err("Not an SPL Token transfer event.".to_string()),
         },
-        _ => Err("Failed to parse instruction.".to_string())
+        _ => Err("Failed to parse instruction.".to_string()),
     }
 }
 
@@ -208,10 +257,12 @@ fn parse_token_mint_to_instruction(
 ) -> Result<spl_token_substream::pb::spl_token::MintToEvent, String> {
     match spl_token_substream::parse_instruction(instruction, context) {
         Ok(Some(event)) => match event {
-            spl_token_substream::pb::spl_token::spl_token_event::Event::MintTo(mint_to) => Ok(mint_to),
-            _ => Err("Not an SPL Token mint to event.".to_string())
+            spl_token_substream::pb::spl_token::spl_token_event::Event::MintTo(mint_to) => {
+                Ok(mint_to)
+            }
+            _ => Err("Not an SPL Token mint to event.".to_string()),
         },
-        _ => Err("Failed to parse instruction.".to_string())
+        _ => Err("Failed to parse instruction.".to_string()),
     }
 }
 
@@ -222,8 +273,8 @@ fn parse_token_burn_instruction(
     match spl_token_substream::parse_instruction(instruction, context) {
         Ok(Some(event)) => match event {
             spl_token_substream::pb::spl_token::spl_token_event::Event::Burn(burn) => Ok(burn),
-            _ => Err("Not an SPL Token burn event.".to_string())
+            _ => Err("Not an SPL Token burn event.".to_string()),
         },
-        _ => Err("Failed to parse instruction".to_string())
+        _ => Err("Failed to parse instruction".to_string()),
     }
 }
