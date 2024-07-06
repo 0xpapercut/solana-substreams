95e7bdc72b894b5e4437751c97db8f81d1560ade
0xpapercut <0xpapercut@gmail.com>
2024-07-06 21:37:31 +0200

    Remove raydium and spl-token files

--- src/raydium_amm/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod state;
-pub mod log;
-pub mod instruction;
-
-pub const RAYDIUM_LIQUIDITY_POOL: &str = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
--- src/spl_token/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod instruction;
-
-pub const SOL_MINT: &str = "So11111111111111111111111111111111111111112";
-pub const TOKEN_PROGRAM: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
