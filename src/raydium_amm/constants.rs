c5a909060f1852a1c629f58ee9e601c81eeb30ce
0xpapercut <0xpapercut@gmail.com>
2024-09-24 23:21:58 +0200

    Update constants

--- src/raydium_amm/constants.rs
+++ src/raydium_amm/constants.rs
@@ -1,6 +1,4 @@
-use lazy_static::lazy_static;
 use substreams_solana_utils::pubkey::Pubkey;
+use substreams_solana::b58;
 
-lazy_static! {
-    pub static ref RAYDIUM_AMM_PROGRAM_ID: Pubkey = Pubkey::from_string("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8");
-}
+pub const RAYDIUM_AMM_PROGRAM_ID: Pubkey = Pubkey(b58!("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"));
