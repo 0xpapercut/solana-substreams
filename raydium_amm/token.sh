fcd4f8deee758c424417211698c67b9d7891718b
0xpapercut <0xpapercut@gmail.com>
2024-09-29 15:15:06 +0200

    Move configuration files to root

--- raydium_amm/token.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export SUBSTREAMS_API_TOKEN=$(curl https://auth.streamingfast.io/v1/auth/issue -s --data-binary '{"api_key":"'$STREAMINGFAST_KEY'"}' | jq -r .token)
-echo "Token set on in SUBSTREAMS_API_TOKEN"
